70aa60c83eb92f54d4174ba4fcabb1b77dc5c5f9
Ruslan <tajimetowruslan@gmail.com>
2025-02-28 08:34:32 +0800

    user Profile page

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,3 +1,29 @@
-export default function ProfilePage() {
-  return <div>ProfilePage</div>;
+import { auth } from '@/auth';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
+export default async function ProfilePage() {
+  const session = await auth();
+  if (!session) {
+    throw new Error('Not authenticated');
+  }
+  return (
+    <div className="flex flex-col items-center justify-center space-y-5">
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle>Profile</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col space-y-2">
+            <div>
+              <span className="font-semibold">Name:</span> {session.user?.name}
+            </div>
+            <div>
+              <span className="font-semibold">Email:</span>{' '}
+              {session.user?.email}
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
